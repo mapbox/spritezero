c8d4ac42d918c7b784075bf0570195a8f5e592a9
allierowan <allie.rowan@mapbox.com>
2017-05-16 13:27:45 -0400

    removed log

--- test/test.js
+++ test/test.js
@@ -181,7 +181,6 @@ test('generateImage unique with empty input', function(t) {
 
 test('generateImage unique with max_size', function(t) {
     spritezero.generateLayoutUnique({ imgs: getFixtures(), pixelRatio: 1, format: false, max_size: 10 }, function(err, layout) {
-        console.log(err);
         t.ok(err);
         t.equal(err.message, 'image created from svg must be 10 pixels or fewer on each side');
         t.end();
